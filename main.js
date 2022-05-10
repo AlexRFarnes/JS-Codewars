69b398d0f7e16cb7bce90cab41920b4c68667a3b
Alex <alexrfarnes@gmail.com>
2022-05-10 17:23:11 +0800

    new challenge complete

--- main.js
+++ main.js
@@ -532,3 +532,21 @@ The program reports the nth day (as an integer) on which the evaporator will be 
 // console.log(sumArray([0, 1, 6, 10, 10])); // ->  17 );
 // console.log(sumArray([-6, -20, -1, -10, -12])); //-> -28 );
 // console.log(sumArray([-6, 20, -1, 10, -12])); // -> 3 );
+
+/**
+ * Beginner Series #2 Clock
+ */
+
+//  Clock shows h hours, m minutes and s seconds after midnight.
+
+//  Your task is to write a function which returns the time since midnight in milliseconds.
+
+function past(h, m, s) {
+  return h * 3600000 + m * 60000 + s * 1000;
+}
+
+console.log(past(0, 1, 1)); // -> 61000
+console.log(past(1, 1, 1)); // -> 3661000
+console.log(past(0, 0, 0)); // -> 0
+console.log(past(1, 0, 1)); // -> 3601000
+console.log(past(1, 0, 0)); // -> 3600000
