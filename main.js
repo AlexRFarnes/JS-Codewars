4fdf6f9f4ca4415fd2983645fb12faa4e7111356
Alex <alexrfarnes@gmail.com>
2022-05-16 09:29:11 +0800

    Solve codewar challenge

--- main.js
+++ main.js
@@ -632,3 +632,36 @@ The program reports the nth day (as an integer) on which the evaporator will be 
 // console.log(duplicateEncode("recede")); // -> "()()()"
 // console.log(duplicateEncode("Success")); // -> ")())())","should ignore case"
 // console.log(duplicateEncode("(( @")); // -> "))(("
+
+/**
+ * Remove the minimum
+ */
+
+// The museum of incredible dull things
+
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+// Task
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// function removeSmallest(numbers) {
+//   const numbersCopy = [...numbers];
+//   const minNumber = [...numbers].sort((a, b) => a - b)[0];
+//   numbersCopy.splice(numbers.indexOf(minNumber), 1);
+//   return numbersCopy;
+// }
+
+// function removeSmallest(numbers) {
+//   return numbers.filter(
+//     (_, idx) => idx !== numbers.indexOf(Math.min(...numbers))
+//   );
+// }
+
+// console.log(removeSmallest([1, 2, 3, 4, 5])); // -> [2, 3, 4, 5]
+// console.log(removeSmallest([5, 3, 2, 1, 4])); // -> [5, 3, 2, 4]
+// console.log(removeSmallest([2, 2, 1, 2, 1])); // -> [2, 2, 2, 1]
+// console.log(removeSmallest([])); // -> []
