0233edc611d4ac2d88196a55de8fa9fa7235772a
Alex <alexrfarnes@gmail.com>
2022-05-23 10:40:09 +0800

    Complete codewars challenge 'Playing with digits'

--- main.js
+++ main.js
@@ -720,3 +720,38 @@ The program reports the nth day (as an integer) on which the evaporator will be 
 // console.log(SeriesSumRecursive(3)); // -> "1.39"
 // console.log(SeriesSum(4)); // -> "1.49"
 // console.log(SeriesSumRecursive(4)); // -> "1.49"
+
+/**
+ * Playing with digits
+ */
+
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+// we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+
+// In other words:
+
+// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+// function digPow(n, p) {
+//   const num =
+//     ("" + n).split("").reduce((sum, num, idx) => (sum += num ** (p + idx)), 0) /
+//     n;
+//   return num % 1 == 0 ? num : -1;
+// }
+
+// console.log(digPow(89, 1)); // -> 1
+// console.log(digPow(92, 1)); // -> -1
+// console.log(digPow(46288, 3)); // -> 51
