6bd599b8451924341e1b4e8de5ed856bcf1477b2
Alex <alexrfarnes@gmail.com>
2022-05-27 09:06:39 +0800

    complete codewars challenge 'Create phone number'

--- main.js
+++ main.js
@@ -815,3 +815,23 @@ console.log(persistence(39)); // -> 3
 console.log(persistence(4)); // -> 0
 console.log(persistence(25)); // -> 2
 console.log(persistence(999)); // -> 4
+
+/**
+ * Create Phone Number
+ */
+
+//  Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+
+function createPhoneNumber(numbers) {
+  string = numbers.join("");
+  const firstPart = string.substring(0, 3);
+  const middlePart = string.substring(3, 6);
+  const endPart = string.substring(6);
+  return `(${firstPart}) ${middlePart}-${endPart}`;
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); //-> "(123) 456-7890"
+console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); //-> "(111) 111-1111"
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); //-> "(123) 456-7890"
