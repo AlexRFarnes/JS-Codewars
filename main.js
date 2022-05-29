33323d2317cf9a103d6e66358e0b4b1d5b1f482d
Alex <alexrfarnes@gmail.com>
2022-05-29 16:10:59 +0800

    Complete codewars challenge 'Build a pile of Cubes'

--- main.js
+++ main.js
@@ -835,3 +835,34 @@ function createPhoneNumber(numbers) {
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); //-> "(123) 456-7890"
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); //-> "(111) 111-1111"
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); //-> "(123) 456-7890"
+
+/**
+ * Build a pile of Cubes
+ */
+
+// Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3, the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+
+// You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+
+// The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+// Examples:
+
+// findNb(1071225) --> 45
+
+// findNb(91716553919377) --> -1
+
+function findNb(m) {
+  let n = 1;
+  let volume = 1;
+  while (volume < m) {
+    n++;
+    volume += n ** 3;
+  }
+  return volume == m ? n : -1;
+}
+
+console.log(findNb(1071225)); // -> 45
+console.log(findNb(4183059834009)); // -> 2022
+console.log(findNb(24723578342962)); // -> -1
+console.log(findNb(135440716410000)); // -> 4824
+console.log(findNb(40539911473216)); // -> 3568
