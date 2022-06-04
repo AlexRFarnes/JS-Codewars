4e20a306447aa7ca22b83115653d659611adcf34
Alex <alexrfarnes@gmail.com>
2022-06-04 14:11:33 +0800

    complete codewars challenge 'Beginner Series #3 Sum of Numbers'

--- main.js
+++ main.js
@@ -1122,3 +1122,31 @@ console.log(tribonacci([3, 2, 1], 10)); // -> [3,2,1,6,9,16,31,56,103,190]
 console.log(tribonacci([1, 1, 1], 1)); // -> [1]
 console.log(tribonacci([300, 200, 100], 0)); // -> []
 console.log(tribonacci([0.5, 0.5, 0.5], 30)); // -> [0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5]
+
+/**
+ * Beginner Series #3 Sum of Numbers
+ */
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+// Examples (a, b) --> output (explanation)
+
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum(a, b) {
+  let sum = 0;
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(getSum(0, -1)); // -> -1
+console.log(getSum(0, 1)); // -> 1
+console.log(getSum(2, 2)); // -> 2
