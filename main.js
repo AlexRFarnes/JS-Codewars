1cd9f9d818ab3ff9ed08525fbc41ddd6466ceb02
Alex <alexrfarnes@gmail.com>
2022-06-06 10:16:50 +0800

    Complete codewars challenge 'Find The Parity Outlier'

--- main.js
+++ main.js
@@ -1174,3 +1174,28 @@ function sortByLength(array) {
 console.log(sortByLength(["Beg", "Life", "I", "To"])); // => ["I", "To", "Beg", "Life"]
 console.log(sortByLength(["", "Moderately", "Brains", "Pizza"])); // => ["", "Pizza", "Brains", "Moderately"]
 console.log(sortByLength(["Longer", "Longest", "Short"])); // => ["Short", "Longer", "Longest"]
+
+/**
+ * Find The Parity Outlier
+ */
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// Examples
+
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+// Should return: 11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21]
+// Should return: 160 (the only even number)
+
+function findOutlier(integers) {
+  return integers.filter(num => num % 2 == 0).length == 1
+    ? integers.filter(num => num % 2 == 0)[0]
+    : integers.filter(num => num % 2 != 0)[0];
+}
+
+console.log(findOutlier([0, 1, 2])); // ->  1
+console.log(findOutlier([1, 2, 3])); // ->  2
+console.log(findOutlier([2, 6, 8, 10, 3])); // ->  3
+console.log(findOutlier([0, 0, 3, 0, 0])); // ->  3
+console.log(findOutlier([1, 1, 0, 1, 1])); // ->  0
