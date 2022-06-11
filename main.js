6883e69c4e00133884373ec57a823157248f5d70
Alex <alexrfarnes@gmail.com>
2022-06-11 16:35:10 +0800

    Complete codewars challenge 'Training JS #5: Basic data types--Object'

--- main.js
+++ main.js
@@ -1400,3 +1400,36 @@ function movie(card, ticket, perc) {
 console.log(movie(500, 15, 0.9)); // -> 43
 console.log(movie(100, 10, 0.95)); // -> 24
 console.log(movie(0, 10, 0.95)); // -> 2
+
+/**
+ * Training JS #5: Basic data types--Object
+ */
+
+// In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
+
+// You can define the object attributes during initialization, like this:
+
+// var animal={name:"dog"}
+
+// you can also set/get some properties after the object definition, like this:
+
+// var animal={}
+// animal.name="dog"  (or animal["name"]="dog")
+
+// Task
+
+// Give you a function animal, accept 1 parameter:obj like this:
+
+// {name:"dog",legs:4,color:"white"}
+
+// and return a string like this:
+
+// "This white dog has 4 legs."
+
+function animal({ name, legs, color }) {
+  return `This ${color} ${name} has ${legs} legs.`;
+}
+
+console.log(animal({ name: "dog", legs: 4, color: "white" })); //-> "This white dog has 4 legs."
+console.log(animal({ name: "cock", legs: 2, color: "red" })); //-> "This red cock has 2 legs."
+console.log(animal({ name: "rabbit", legs: 4, color: "gray" })); //-> "This gray rabbit has 4 legs."
