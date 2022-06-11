1f5ce0647bb99cf0d2e56b20ff38f13757bf4ed0
Alex <alexrfarnes@gmail.com>
2022-06-11 16:35:40 +0800

    Complete codewars challenge 'Sum of Digits / Digital Root'

--- main.js
+++ main.js
@@ -1433,3 +1433,42 @@ function animal({ name, legs, color }) {
 console.log(animal({ name: "dog", legs: 4, color: "white" })); //-> "This white dog has 4 legs."
 console.log(animal({ name: "cock", legs: 2, color: "red" })); //-> "This red cock has 2 legs."
 console.log(animal({ name: "rabbit", legs: 4, color: "gray" })); //-> "This gray rabbit has 4 legs."
+
+/**
+ * Sum of Digits / Digital Root
+ */
+
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+// Examples
+
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+// If else recursive version
+// function digital_root(n) {
+//   if (String(n).length == 1) return n;
+//   else {
+//     n = String(n)
+//       .split("")
+//       .reduce((acc, num) => acc + +num, 0);
+//     return digital_root(n);
+//   }
+// }
+
+// Ternary operator recursive version
+function digital_root(n) {
+  return String(n).length == 1
+    ? n
+    : digital_root(
+        String(n)
+          .split("")
+          .reduce((acc, num) => acc + +num, 0)
+      );
+}
+
+console.log(digital_root(16)); // -> 7
+console.log(digital_root(456)); // -> 6
