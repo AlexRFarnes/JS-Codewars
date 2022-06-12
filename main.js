97b7cb486b445d526f826ad3bde40b1d7f78a545
Alex <alexrfarnes@gmail.com>
2022-06-12 17:35:24 +0800

    Complete codewars challenge 'Replace With Alphabet Position'

--- main.js
+++ main.js
@@ -1472,3 +1472,32 @@ function digital_root(n) {
 
 console.log(digital_root(16)); // -> 7
 console.log(digital_root(456)); // -> 6
+
+/**
+ * Replace With Alphabet Position
+ */
+
+//  Welcome.
+
+//  In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+//  If anything in the text isn't a letter, ignore it and don't return it.
+
+//  "a" = 1, "b" = 2, etc.
+//  Example
+
+//  alphabetPosition("The sunset sets at twelve o' clock.")
+
+//  Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+  return text
+    .toLowerCase()
+    .split("")
+    .filter(char => char.charCodeAt() > 96 && char.charCodeAt() < 123)
+    .map(char => char.charCodeAt() - 96)
+    .join(" ");
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // -> "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition("The narwhal bacons at midnight.")); // -> "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
