2bc1fcf57be619478fc89a1718b51146d8f4ccdc
Alex <alexrfarnes@gmail.com>
2022-06-13 10:23:44 +0800

    Complete codewars challenge 'Is this a triangle?'

--- main.js
+++ main.js
@@ -1501,3 +1501,18 @@ function alphabetPosition(text) {
 
 console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // -> "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
 console.log(alphabetPosition("The narwhal bacons at midnight.")); // -> "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+
+/**
+ * Is this a triangle?
+ */
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && b + c > a;
+}
+
+console.log(isTriangle(1, 2, 2)); // ->  true
+console.log(isTriangle(7, 2, 2)); // ->  false
