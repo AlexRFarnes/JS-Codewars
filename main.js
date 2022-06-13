4db9db8423aa43becfe7c7d3c636d7f4ef13c341
Alex <alexrfarnes@gmail.com>
2022-06-14 07:52:28 +0800

    Complete codewars challenge 'Count of positives / sum of negatives'

--- main.js
+++ main.js
@@ -1516,3 +1516,35 @@ function isTriangle(a, b, c) {
 
 console.log(isTriangle(1, 2, 2)); // ->  true
 console.log(isTriangle(7, 2, 2)); // ->  false
+
+/**
+ * Count of positives / sum of negatives
+ */
+
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+// Example
+
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length == 0) return [];
+  return [
+    input.filter(num => num > 0).length,
+    input.reduce((sum, num) => (sum += num < 0 ? num : 0), 0),
+  ];
+}
+
+console.log(
+  countPositivesSumNegatives([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
+  ])
+); // [10, -65]
+console.log(
+  countPositivesSumNegatives([
+    0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
+  ])
+); // [8, -50]
