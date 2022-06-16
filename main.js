d1e35b3b41e118ed3d4bc4c8e225e3ae0ace7db0
Alex <alexrfarnes@gmail.com>
2022-06-16 11:28:03 +0800

    Complete codewars challenge 'Mumbling'

--- main.js
+++ main.js
@@ -1548,3 +1548,28 @@ console.log(
     0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14,
   ])
 ); // [8, -50]
+
+/**
+ * Mumbling
+ */
+
+// This time no story, no theory. The examples below show you how to write function accum:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  return s
+    .toUpperCase()
+    .split("")
+    .map((char, idx) => char + char.toLowerCase().repeat(idx))
+    .join("-");
+}
+
+console.log(accum("ZpglnRxqenU")); // -> "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum("NyffsGeyylB")); // -> "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum("MjtkuBovqrU")); // -> "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+console.log(accum("EvidjUnokmM")); // -> "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+console.log(accum("HbideVbxncC")); // -> "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
