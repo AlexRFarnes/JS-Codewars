879abb49481d27685ea93509c5fa1b1a00d0fc04
Alex <alexrfarnes@gmail.com>
2022-06-17 08:38:58 +0800

    Complete codewars challenge 'Find the unique number'

--- main.js
+++ main.js
@@ -1653,3 +1653,35 @@ function grow(x) {
 console.log(grow([1, 2, 3])); // -> 6
 console.log(grow([4, 1, 1, 1, 4])); // -> 16
 console.log(grow([2, 2, 2, 2, 2, 2])); // -> 64
+
+/**
+ * Find the unique number
+ */
+
+//  There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+//  findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+//  findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+
+//  It’s guaranteed that array contains at least 3 numbers.
+
+//  The tests contain some very huge arrays, so think about performance.
+
+// function findUniq(arr) {
+//   let uniq;
+//   arr.forEach(el => {
+//     if (arr.indexOf(el) == arr.lastIndexOf(el)) uniq = el;
+//   });
+//   return uniq;
+// }
+
+function findUniq(arr) {
+  return arr.find(el => arr.indexOf(el) == arr.lastIndexOf(el));
+}
+
+console.log(findUniq([1, 0, 0])); //-> 1
+console.log(findUniq([0, 1, 0])); //-> 1
+console.log(findUniq([0, 0, 1])); //-> 1
+console.log(findUniq([1, 1, 1, 2, 1, 1])); //-> 2
+console.log(findUniq([1, 1, 2, 1, 1])); //-> 2
+console.log(findUniq([3, 10, 3, 3, 3])); //-> 10
