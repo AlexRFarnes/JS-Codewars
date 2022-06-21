12a9f399f3e0abfd5f98fdca867f39b9d9d942df
Alex <alexrfarnes@gmail.com>
2022-06-21 11:19:33 +0800

    Complete codewars challenge 'Century From Year'

--- main.js
+++ main.js
@@ -1795,3 +1795,31 @@ class Kata {
 
 console.log(Kata.getVolumeOfCuboid(1, 2, 2)); // -> 4
 console.log(Kata.getVolumeOfCuboid(6.3, 2, 5)); // -> 63
+
+/**
+ * Century From Year
+ */
+
+// ntroduction
+
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+// Task
+
+// Given a year, return the century it is in.
+// Examples
+
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+  // Finish this :)
+  return Math.ceil(year / 100);
+}
+
+console.log(century(1705)); // -> 18
+console.log(century(1900)); // -> 19
+console.log(century(1601)); // -> 17
+console.log(century(2000)); // -> 20
+console.log(century(89)); // -> 1
