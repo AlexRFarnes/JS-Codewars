1e216612f0632cbed6effa429670d461dc1607f3
Alex <alexrfarnes@gmail.com>
2022-06-21 11:16:16 +0800

    Complete codewars challenge 'Volume of a Cuboid'

--- main.js
+++ main.js
@@ -1780,3 +1780,18 @@ function isPangram(string) {
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog.")); // => true
 console.log(isPangram("This is not a pangram.")); // => false
+
+/**
+ * Volume of a Cuboid
+ */
+
+//  Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+console.log(Kata.getVolumeOfCuboid(1, 2, 2)); // -> 4
+console.log(Kata.getVolumeOfCuboid(6.3, 2, 5)); // -> 63
