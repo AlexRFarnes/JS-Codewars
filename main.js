7c5874ae491e75f0f6c40b224e2f57b1650c6873
Alex <alexrfarnes@gmail.com>
2022-06-23 10:46:10 +0800

    Complete codewars challenge 'Total amount of points'

--- main.js
+++ main.js
@@ -1891,10 +1891,60 @@ console.log(inArray(["tarp", "mice", "bull"], a2)); // ->  []
 function arrayPlusArray(arr1, arr2) {
   return (
     arr1.reduce((sum, num) => sum + num) + arr2.reduce((sum, num) => sum + num)
-  ); //something went wrong
+  );
 }
 
 console.log(arrayPlusArray([1, 2, 3], [4, 5, 6])); // -> 21
 console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6])); // -> -21
 console.log(arrayPlusArray([0, 0, 0], [4, 5, 6])); // -> 15
 console.log(arrayPlusArray([100, 200, 300], [400, 500, 600])); // -> 2100
+
+/**
+ * Total amount of points
+ */
+
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+//     if x > y: 3 points
+//     if x < y: 0 point
+//     if x = y: 1 point
+
+// Notes:
+
+//     there are 10 matches in the championship
+//     0 <= x <= 4
+//     0 <= y <= 4
+
+function points(games) {
+  return games
+    .map(game => {
+      if (game.split(":")[0] > game.split(":")[1]) {
+        return 3;
+      } else if (game.split(":")[0] < game.split(":")[1]) {
+        return 0;
+      } else {
+        return 1;
+      }
+    })
+    .reduce((sum, num) => sum + num);
+}
+
+console.log(
+  points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"])
+); // -> 30
+console.log(
+  points(["1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4"])
+); // -> 10
+console.log(
+  points(["0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4"])
+); // -> 0
+console.log(
+  points(["1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4"])
+); // -> 15
+console.log(
+  points(["1:0", "2:0", "3:0", "4:4", "2:2", "3:3", "1:4", "2:3", "2:4", "3:4"])
+); // -> 12
