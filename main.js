d83170afd3b196000bbc9e995ba6764f615715c4
Alex <alexrfarnes@gmail.com>
2022-06-29 13:38:09 +0800

    Complete codewars challenge 'altERnaTIng cAsE <=> ALTerNAtiNG CaSe'

--- main.js
+++ main.js
@@ -2097,3 +2097,43 @@ function longest(s1, s2) {
 console.log(longest("aretheyhere", "yestheyarehere")); // -> "aehrsty"
 console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); // -> "abcdefghilnoprstu"
 console.log(longest("inmanylanguages", "theresapairoffunctions")); // -> "acefghilmnoprstuy"
+
+/**
+ * altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+ */
+
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map(char => {
+      if (char >= "A" && char <= "Z") {
+        return char.toLowerCase();
+      } else if (char >= "a" && char <= "z") {
+        return char.toUpperCase();
+      } else return char;
+    })
+    .join("");
+};
+
+console.log("hello world".toAlternatingCase()); // -> "HELLO WORLD"
+console.log("HELLO WORLD".toAlternatingCase()); // -> "hello world"
+console.log("hello WORLD".toAlternatingCase()); // -> "HELLO world"
+console.log("HeLLo WoRLD".toAlternatingCase()); // -> "hEllO wOrld"
+console.log("12345".toAlternatingCase()); // -> "12345"
+console.log("1a2b3c4d5e".toAlternatingCase()); // -> "1A2B3C4D5E"
+console.log("String.prototype.toAlternatingCase".toAlternatingCase()); // -> "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+console.log("Hello World".toAlternatingCase().toAlternatingCase()); // -> "Hello World"
