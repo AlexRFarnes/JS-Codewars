620d3c8dba680e1998d71ca9d3b1e1a76afc7f09
Alex <alexrfarnes@gmail.com>
2022-07-02 08:27:53 +0800

    Complete codewars challenge 'Round up to the next multiple of 5'

--- main.js
+++ main.js
@@ -2191,3 +2191,43 @@ function square(num) {
 }
 
 console.log(square(3)); // -> 9
+
+/**
+ * Calculating with Functions
+ */
+
+/**
+ * Round up to the next multiple of 5
+ */
+
+//  Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+//  Examples:
+
+//  input:    output:
+//  0    ->   0
+//  2    ->   5
+//  3    ->   5
+//  12   ->   15
+//  21   ->   25
+//  30   ->   30
+//  -2   ->   0
+//  -5   ->   -5
+//  etc.
+
+//  Input may be any positive or negative integer (including 0).
+
+//  You can assume that all inputs are valid integers.
+
+function roundToNext5(n) {
+  return Math.ceil(n / 5) * 5;
+}
+
+console.log(roundToNext5(0)); // -> 0
+console.log(roundToNext5(1)); // -> 5
+console.log(roundToNext5(-1)); // -> 0
+console.log(roundToNext5(-5)); // -> -5
+console.log(roundToNext5(3)); // -> 5
+console.log(roundToNext5(5)); // -> 5
+console.log(roundToNext5(20)); // -> 20
+console.log(roundToNext5(39)); // -> 40
