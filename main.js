71aebaf14b106a7bf37b3e2d49dc56139e3cb606
Alex <alexrfarnes@gmail.com>
2022-07-04 09:25:46 +0800

    Complete codewars challenge 'Anagram Detection'

--- main.js
+++ main.js
@@ -2257,3 +2257,34 @@ var findMissing = function (list) {
 console.log(findMissing([1, 3, 4])); // => 2
 console.log(findMissing([-75, -25, 0, 25])); // -50
 console.log(findMissing([-75, -25, 75])); // 25
+
+/**
+ * Anagram Detection
+ */
+
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+// Examples
+
+//     "foefet" is an anagram of "toffee"
+
+//     "Buckethead" is an anagram of "DeathCubeK"
+
+// write the function isAnagram
+var isAnagram = function (test, original) {
+  return (
+    test.toLowerCase().split("").sort().join("") ===
+    original.toLowerCase().split("").sort().join("")
+  );
+};
+
+console.log(isAnagram("foefet", "toffee")); // ->  true
+console.log(isAnagram("Buckethead", "DeathCubeK")); // ->  true
+console.log(isAnagram("Twoo", "WooT")); // ->  true
+
+console.log(isAnagram("dumble", "bumble")); // ->  false
+console.log(isAnagram("ound", "round")); // ->  false
+console.log(isAnagram("apple", "pale")); // ->  false
