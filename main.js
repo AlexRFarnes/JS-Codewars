b65027ddad913fff00e528e754b4667de1290d9f
alex <alexrfarnes@gmail.com>
2022-07-06 15:13:47 +0800

    Complete codewars challenge 'Multiples of 3 or 5'

--- main.js
+++ main.js
@@ -2315,3 +2315,24 @@ console.log(rentalCarCost(7)); // -> 230
 console.log(rentalCarCost(8)); // -> 270
 console.log(rentalCarCost(9)); // -> 310
 console.log(rentalCarCost(10)); // -> 350
+
+/**
+ * Multiples of 3 or 5
+ */
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(number) {
+  let sum = 0;
+  if (number < 0) return sum;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) sum += i;
+  }
+  return sum;
+}
+
+console.log(solution(10)); // -> 23
