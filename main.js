2726bcbccbb32bf7756d38591f167d3f93f625b8
Alex <alexrfarnes@gmail.com>
2022-07-13 15:14:07 +0800

    Complete codewars challenge 'Your order, please'

--- main.js
+++ main.js
@@ -2480,3 +2480,32 @@ console.log(
 ); // -> 'false'
 console.log(isValidWalk(["w"])); // -> 'false'
 console.log(isValidWalk(["n", "n", "n", "s", "n", "s", "n", "s", "n", "s"])); // -> 'false'
+
+/**
+ * Your order, please
+ */
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  const wordsArr = words.split(" ");
+  const orderedWords = [];
+  for (let i = 1; i <= wordsArr.length; i++) {
+    orderedWords.push(wordsArr.find(word => word.includes(`${i}`)));
+  }
+
+  return orderedWords.join(" ");
+}
+
+console.log(order("is2 Thi1s T4est 3a")); // -> "Thi1s is2 3a T4est"
+console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // -> "Fo1r the2 g3ood 4of th5e pe6ople"
+console.log(order("")); // -> ""
