9d64e6101221589036d14155fd49373042e32215
Alex <alexrfarnes@gmail.com>
2022-07-14 13:35:17 +0800

    Complete codewars challenge 'Credit Card Mask'

--- main.js
+++ main.js
@@ -2509,3 +2509,34 @@ function order(words) {
 console.log(order("is2 Thi1s T4est 3a")); // -> "Thi1s is2 3a T4est"
 console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // -> "Fo1r the2 g3ood 4of th5e pe6ople"
 console.log(order("")); // -> ""
+
+/**
+ * Credit Card Mask
+ */
+
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+// Examples
+
+// "4556364607935616" --> "############5616"
+//      "64607935616" -->      "#######5616"
+//                "1" -->                "1"
+//                 "" -->                 ""
+
+// // "What was the name of your first pet?"
+
+// "Skippy" --> "##ippy"
+
+// "Nananananananananananananananana Batman!"
+// -->
+// "####################################man!"
+
+// return masked string
+function maskify(cc) {
+  return Array(cc.slice(0, -4).length).fill("#").join("") + cc.slice(-4);
+}
+
+console.log(maskify("4556364607935616")); // ->  "############5616"
+console.log(maskify("1")); // ->  "1"
+console.log(maskify("11111")); // ->  "#1111"
