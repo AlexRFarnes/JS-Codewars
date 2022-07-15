008b46e676fbb249ef527cf1c5a89fec76b52631
Alex <alexrfarnes@gmail.com>
2022-07-15 17:11:36 +0800

    Complete codewars challenge 'Find the divisors\!'

--- main.js
+++ main.js
@@ -2540,3 +2540,28 @@ function maskify(cc) {
 console.log(maskify("4556364607935616")); // ->  "############5616"
 console.log(maskify("1")); // ->  "1"
 console.log(maskify("11111")); // ->  "#1111"
+
+/**
+ * Find the divisors!
+ */
+
+// // Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+// Example:
+
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+function divisors(integer) {
+  const divisors = [];
+
+  for (let i = 2; i < integer; i++) {
+    if (integer % i == 0) divisors.push(i);
+  }
+
+  return divisors.length == 0 ? `${integer} is prime` : divisors;
+}
+
+console.log(divisors(15)); // ->  [3, 5]
+console.log(divisors(12)); // ->  [2, 3, 4, 6]
+console.log(divisors(13)); // ->  "13 is prime"
