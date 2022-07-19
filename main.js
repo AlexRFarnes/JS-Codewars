097e65b97891c018502278915625b20b7b262add
Alex <alexrfarnes@gmail.com>
2022-07-19 18:01:57 +0800

    Complete challenge 'Chain Me'

--- main.js
+++ main.js
@@ -2620,3 +2620,46 @@ function greet(name, owner) {
 
 console.log(greet("Daniel", "Daniel")); // ->  'Hello boss'
 console.log(greet("Greg", "Daniel")); // ->  'Hello guest'
+
+/**
+ * Chain Me
+ */
+
+// Write a generic function chainer
+
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+// function add(num) {
+//   return num + 1;
+// }
+
+// function mult(num) {
+//   return num * 30;
+// }
+
+// chain(2, [add, mult]);
+// // returns 90;
+
+function chain(input, fs) {
+  for (const fn of fs) {
+    input = fn(input);
+  }
+  return input;
+}
+
+function chainReducer(input, fs) {
+  return fs.reduce((input, fn) => fn(input), input);
+}
+
+function add(x) {
+  return x + 10;
+}
+
+function mult(x) {
+  return x * 30;
+}
+
+console.log(chain(2, [add, mult])); // -> 360
+console.log(chainReducer(2, [add, mult])); // -> 360
