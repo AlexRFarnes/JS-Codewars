d3e146f97c8e107c157dae9a624f23ca8a756282
Alex <alexrfarnes@gmail.com>
2022-07-22 11:13:53 +0800

    Complete challenge 'Is it even?'

--- main.js
+++ main.js
@@ -2676,3 +2676,27 @@ function reverseList(arr) {
 
 console.log(reverseList([])); // -> []
 console.log(reverseList([1, 2, 3])); // -> [3,2,1]
+
+/**
+ * Is it even?
+ */
+
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+function testEven(n) {
+  return n % 2 == 0;
+}
+
+console.log(testEven(0)); // -> true
+console.log(testEven(0.5)); // -> false
+console.log(testEven(1)); // -> false
+console.log(testEven(2)); // -> true
+console.log(testEven(-4)); // -> true
